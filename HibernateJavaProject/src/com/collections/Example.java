d459ea8d76c98e85de90f10ff088de384c7a1b5c
Mahesh90918 <mahesh996345@gmail.com>
2022-12-18 15:36:59 +0530

    ghuio

---
+++ HibernateJavaProject/src/com/collections/Example.java
@@ -0,0 +1,15 @@
+package com.collections;
+
+import java.util.LinkedList;
+
+public class Example {
+	public static void main(String[] args) {
+		LinkedList<String> li = new LinkedList<>();
+		li.add("ganesh");
+		li.add("mahesh");
+		li.add("siva");
+		li.add(null);
+		li.add("subbu");
+		System.out.println(li);
+	}
+}
