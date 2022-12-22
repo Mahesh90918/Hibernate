f5b5dd771996248180fcf43bdd5f1bba60ae2b32
Mahesh90918 <mahesh996345@gmail.com>
2022-12-22 12:11:12 +0530

    mapping

--- MappingsStudent/src/main/java/com/StudentTest/RetrieveDataStudent.java
+++ MappingsStudent/src/main/java/com/StudentTest/RetrieveDataStudent.java
@@ -1,23 +1,27 @@
 package com.StudentTest;
 
 import java.util.List;
-import java.util.Set;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 import com.model.Student;
-import com.model.Teacher;
 import com.util.SessionUtil;
 
 public class RetrieveDataStudent {
 	public static void main(String[] args) {
 		Session session = SessionUtil.getSession();
-		Student list = (Student) session.get(Student.class, 1);
+//		Student list = (Student) session.get(Student.class, 1);
 //		Set<Teacher> set = list.getTeacher();
 //		for (Teacher teacher : set) {
 //			System.out.println(teacher.getTid());
 //			System.out.println(teacher.getTname());
 //		}
+		Query query = session.createQuery("Select s from Student s");
+		List<Student> list = query.list();
+		for (Student student : list) {
+			System.out.println(student.getAddress());
+		}
 		session.close();
 	}
 }
